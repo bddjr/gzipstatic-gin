2ea7d7b2bac27efd14e80f19162fccd051fedd63
bddjr <bddjrcn@outlook.com>
2024-09-25 23:27:17 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# gzipstatic-gin
+
+使用静态gzip或br压缩文件响应，减少服务器性能占用。
+
+Use static gzip or br compression file response to reduce server performance consumption.
+
+## Get
+```
+go get github.com/bddjr/gzipstatic-gin
+```
---
+++ gzipstatic.go
@@ -0,0 +1,153 @@
+package gzipstatic
+
+import (
+	"mime"
+	"net/http"
+	"path"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+var ExtFillter = regexp.MustCompile(`\.(html|htm|js|json|css)$`)
+
+type EncodeListItem struct {
+	name string
+	ext  string
+}
+
+// Priority from low to high
+var EncodeList = []EncodeListItem{
+	{
+		name: "gzip",
+		ext:  ".gz",
+	}, {
+		name: "br",
+		ext:  ".br",
+	},
+}
+
+var NoRoute = func(ctx *gin.Context) {
+	ctx.Writer.WriteString("404 page not found")
+}
+
+func fileFromFS(ctx *gin.Context, name string, fs http.FileSystem) (next bool) {
+	if strings.HasSuffix(ctx.Request.URL.Path, "/index.html") {
+		ctx.Header("Location", "../")
+		ctx.Status(301)
+		return false
+	}
+
+	if name == "" || strings.HasSuffix(name, "/") {
+		name += "index.html"
+	}
+
+	ext := ExtFillter.FindString(name)
+	if ext == "" {
+		return true
+	}
+
+	// Select encoding based on priority
+	n := -1
+	for _, name := range strings.Split(ctx.GetHeader("Accept-Encoding"), ", ") {
+		name, _, _ = strings.Cut(name, ";")
+		for i := n + 1; i < len(EncodeList); i++ {
+			if name == EncodeList[i].name {
+				n = i
+			}
+		}
+		if n == len(EncodeList)-1 {
+			break
+		}
+	}
+
+	// try
+	for ; n > -1; n-- {
+		f, err := fs.Open(name + EncodeList[n].ext)
+		if err != nil {
+			continue
+		}
+		defer f.Close()
+
+		s, err := f.Stat()
+		if err != nil || s.IsDir() {
+			continue
+		}
+
+		h := ctx.Writer.Header()
+		h.Del("Content-Length")
+		h.Add("Vary", "Accept-Encoding")
+		h.Set("X-Content-Encoding-By", "gzipstatic-gin")
+		h.Set("Content-Encoding", EncodeList[n].name)
+		h.Set("Content-Type", mime.TypeByExtension(ext))
+
+		http.ServeContent(ctx.Writer, ctx.Request, name, s.ModTime(), f)
+		return false
+	}
+	return true
+}
+
+func File(ctx *gin.Context, FilePath string) {
+	dir, name := filepath.Split(FilePath)
+	if fileFromFS(ctx, name, http.Dir(dir)) {
+		ctx.File(FilePath)
+	}
+}
+
+func FileFromFS(ctx *gin.Context, name string, fs http.FileSystem) {
+	if fileFromFS(ctx, name, fs) {
+		ctx.FileFromFS(name, fs)
+	}
+}
+
+func staticFileHandler(group gin.IRoutes, relativePath string, handler gin.HandlerFunc) gin.IRoutes {
+	if strings.Contains(relativePath, ":") || strings.Contains(relativePath, "*") {
+		panic("URL parameters can not be used when serving a static file")
+	}
+	group.GET(relativePath, handler)
+	group.HEAD(relativePath, handler)
+	return group
+}
+
+func StaticFile(group gin.IRoutes, relativePath, filepath string) gin.IRoutes {
+	return staticFileHandler(group, relativePath, func(ctx *gin.Context) {
+		File(ctx, filepath)
+	})
+}
+func StaticFileFS(group gin.IRoutes, relativePath, filepath string, fs http.FileSystem) gin.IRoutes {
+	return staticFileHandler(group, relativePath, func(ctx *gin.Context) {
+		FileFromFS(ctx, filepath, fs)
+	})
+}
+
+func Static(group gin.IRoutes, relativePath, root string) gin.IRoutes {
+	return StaticFS(group, relativePath, gin.Dir(root, false))
+}
+
+func StaticFS(group gin.IRoutes, relativePath string, fs http.FileSystem) gin.IRoutes {
+	if strings.Contains(relativePath, ":") || strings.Contains(relativePath, "*") {
+		panic("URL parameters can not be used when serving a static folder")
+	}
+	handler := func(ctx *gin.Context) {
+		name := ctx.Param("filepath")
+		if fileFromFS(ctx, name, fs) {
+			f, err := fs.Open(name)
+			if err != nil {
+				ctx.Status(404)
+				if NoRoute != nil {
+					NoRoute(ctx)
+				}
+				return
+			}
+			f.Close()
+			ctx.FileFromFS(name, fs)
+		}
+	}
+	urlPattern := path.Join(relativePath, "/*filepath")
+
+	group.GET(urlPattern, handler)
+	group.HEAD(urlPattern, handler)
+	return group
+}
